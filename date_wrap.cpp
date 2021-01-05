cf9137f20b603ae8f7aaf276c0d093a4aad79fe2
Itay Segev <itaysegev11@gmail.com>
2021-01-05 20:53:50 +0200

    Ctor Dtor COPYtor =+ ++ << operators

--- date_wrap.cpp
+++ date_wrap.cpp
@@ -1,12 +1,19 @@
 #include "date.h"
+#include <iostream>
 class DateWrap{
     private:
         Date date;
 
     public:
+        DateWrap(int day,int month,int year);
+        ~DateWrap();
+        DateWarp(const DateWarp& date_to_copy);
         int year() const;
         int month() const;  
         int day() const;
+        DateWrap& operator+=(const int days);
+        DateWrap& operator++();
+        bool operator==(const Date& date2) const;
         bool operator==(const Date& date2) const;
         bool operator>(const Date& date2) const;
         bool operator<(const Date& date2) const;
@@ -14,7 +21,16 @@ class DateWrap{
         bool operator<=(const Date& date2) const;
         bool operator!=(const Date& date2) const;
 };
-
+DateWrap::DateWarp(int day,int month,int year){
+    date=dateCreate(day,month,year);
+    //should add invalid date exception 
+}
+DateWrap::~DateWarp(){
+    dateDestroy(date)
+}
+DateWarp(const DateWarp& date_to_copy){
+    date=dateCopy(date_to_copy);
+}
 int DateWrap::year() const{
     int day,month,year;
     dateGet(date,&day,&month,&year);
@@ -31,6 +47,15 @@ int DateWrap::day() const{
     return day;
 }
 
+DateWrap& DateWarp::operator++(){
+    dateTick(date);
+}
+DateWrap& DateWarp::operator+=(const int days){
+    //should add exception 
+    for(int i=0;i<days;i++){
+        dateTick(date);
+    }
+}
 bool DateWrap::operator==(const Date& date2) const{
     if(dateCompare(date,date2)==0){
         return true;
@@ -78,4 +103,8 @@ bool DateWrap::operator<=(const Date& date2) const{
 
 bool DateWrap::operator!=(const Date& date2) const{
     return !(date==date2);
+}
+
+ostream& operator<<(ostream& os,const DateWarp& date){
+    return os<<day()<<"/"<<month()<<"/"<<year()<<"\n",
 }
