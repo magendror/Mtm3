63ffb19fc59a3bc6154f9e9fb73dc02d1eddaccc
magendror <magendror@gmail.com>
2021-01-07 20:34:48 +0200

    changes for debugging plus adding assighment operator

--- date_wrap.cpp
+++ date_wrap.cpp
@@ -1,4 +1,6 @@
-#include "date.h"
+extern "C"{
+    #include "date.h"
+}
 #include "date_wrap.h"
 #include <iostream>
 
@@ -8,106 +10,122 @@ using std::cerr;
 using std::cin;
 using std::endl;
 
+namespace mtm{
 
+    DateWrap::DateWrap(int day,int month,int year):
+        date(dateCreate(day,month,year)){
+        //should add invalid date exception 
+    }
 
-DateWrap::DateWrap(int day,int month,int year):
-    date(dateCreate(day,month,year)){
-    //should add invalid date exception 
-}
-DateWrap::~DateWrap(){
-    dateDestroy(date);
-}
-//problem because of const!!
-DateWrap::DateWrap(const DateWrap& date_to_copy){
-    DateWrap(date_to_copy.day(),date_to_copy.month(),date_to_copy.year());
-}
-int DateWrap::year() const{
-    int day,month,year;
-    dateGet(date,&day,&month,&year);
-    return year;
-}
-int DateWrap::month() const{
-    int day,month,year;
-    dateGet(date,&day,&month,&year);
-    return month;
-}    
-int DateWrap::day() const{
-    int day,month,year;
-    dateGet(date,&day,&month,&year);
-    return day;
-}
+    DateWrap::~DateWrap(){
+        dateDestroy(date);
+    }
 
-DateWrap& DateWrap::operator++(int){
-    dateTick(date);
-    return *this;
-}
-DateWrap& DateWrap::operator+=(const int days){
-    //should add exception 
-    for(int i=0;i<days;i++){
+    DateWrap::DateWrap(const DateWrap& date_to_copy):
+        date(dateCreate(date_to_copy.day(),date_to_copy.month(),date_to_copy.year())){
+    }
+
+    DateWrap& DateWrap::operator=(const DateWrap& date_to_copy){
+        if (this == &date_to_copy) {
+		    return *this;
+	    }
+        dateDestroy(date);
+        date = dateCreate(date_to_copy.day(),date_to_copy.month(),date_to_copy.year());
+        return *this;
+
+    }
+
+    int DateWrap::year() const{
+        int day,month,year;
+        dateGet(date,&day,&month,&year);
+        return year;
+    }
+    int DateWrap::month() const{
+        int day,month,year;
+        dateGet(date,&day,&month,&year);
+        return month;
+    }    
+    int DateWrap::day() const{
+        int day,month,year;
+        dateGet(date,&day,&month,&year);
+        return day;
+    }
+
+    DateWrap& DateWrap::operator++(int){
         dateTick(date);
+        return *this;
     }
-}
-bool DateWrap::operator==(const DateWrap& date_wrap2) const{
-    if(dateCompare(date,date_wrap2.date)==0){
-        return true;
+    DateWrap& DateWrap::operator+=(const int days){
+        //should add exception 
+        for(int i=0;i<days;i++){
+            dateTick(date);
+        }
+        return *this;
     }
-    else{
-        return false;
-    }
-}
 
-bool DateWrap::operator<(const DateWrap& date_wrap2) const{
-    if(dateCompare(date,date_wrap2.date)<0){
-        return true;
+    bool DateWrap::operator==(const DateWrap& date_wrap2) const{
+        if(dateCompare(date,date_wrap2.date)==0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    else{
-        return false;
-    }
-}
 
-bool DateWrap::operator>(const DateWrap& date_wrap2) const{
-    if(dateCompare(date,date_wrap2.date)>0){
-        return true;
+    bool DateWrap::operator<(const DateWrap& date_wrap2) const{
+        if(dateCompare(date,date_wrap2.date)<0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    else{
-        return false;
-    }
-}
 
-bool DateWrap::operator>=(const DateWrap& date_wrap2) const{
-    if(date>date_wrap2.date||date==date_wrap2.date){
-        return true;
+    bool DateWrap::operator>(const DateWrap& date_wrap2) const{
+        if(dateCompare(date,date_wrap2.date)>0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    else{
-        return false;
+
+    bool DateWrap::operator>=(const DateWrap& date_wrap2) const{
+        if((date>date_wrap2.date)||(date==date_wrap2.date)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-}
 
-bool DateWrap::operator<=(const DateWrap& date_wrap2) const{
-    if(date<date_wrap2.date||date==date_wrap2.date){
-        return true;
+    bool DateWrap::operator<=(const DateWrap& date_wrap2) const{
+        if((date<date_wrap2.date)||(date==date_wrap2.date)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
-    else{
-        return false;
+
+    bool DateWrap::operator!=(const DateWrap& date_wrap2) const{
+        return !(date==date_wrap2.date);
     }
-}
+    //need to add to public with a friend
+    ostream& operator<<(ostream& os,const DateWrap& date){
+        return os<<date.day()<<"/"<<date.month()<<"/"<<date.year()<<"\n";
+    }
 
-bool DateWrap::operator!=(const DateWrap& date_wrap2) const{
-    return !(date==date_wrap2.date);
-}
-//need to add to public with a friend
-ostream& operator<<(ostream& os,const DateWrap& date){
-    return os<<date.day()<<"/"<<date.month()<<"/"<<date.year()<<"\n";
-}
+    DateWrap operator+(const DateWrap& date_wrap,const int& days){
+        DateWrap new_date = DateWrap(date_wrap);
+        new_date+=days;
+        return new_date;
+    }
 
-DateWrap& operator+(const DateWrap& date_wrap,const int& days){
-    DateWrap new_date(date_wrap);
-    new_date+=days;
-    return new_date;
-}
+    DateWrap operator+(const int& days,const DateWrap& date_wrap) {
+        DateWrap new_date = DateWrap(date_wrap);
+        new_date+=days;
+        return new_date;
+    }
 
-DateWrap& operator+(const int& days,const DateWrap& date_wrap) {
-    DateWrap new_date(date_wrap);
-    new_date+=days;
-    return new_date;
 }
--- date_wrap.h
+++ date_wrap.h
@@ -1,33 +1,46 @@
 #ifndef DATE_WRAP_H_
 #define DATE_WRAP_H_
 
-#include "date.h"
+extern "C"{
+    #include "date.h"
+}
+#include <iostream>
 
-class DateWrap{
-    private:
-        Date date;
+using std::ostream;
+using std::cout;
+using std::cerr;
+using std::cin;
+using std::endl;
 
-    public:
-        DateWrap(int day,int month,int year);
-        ~DateWrap();
-        DateWrap(const DateWrap& date_to_copy);
-        int year() const;
-        int month() const;  
-        int day() const;
-        DateWrap& operator+=(const int days);
-        DateWrap& operator++(int);
-        bool operator==(const DateWrap& date2) const;
-        bool operator==(const DateWrap& date2) const;
-        bool operator>(const DateWrap& date2) const;
-        bool operator<(const DateWrap& date2) const;
-        bool operator>=(const DateWrap& date2) const;
-        bool operator<=(const DateWrap& date2) const;
-        bool operator!=(const DateWrap& date2) const;
-        friend ostream& operator<<(ostream& os, const DateWrap& date);
-};
+namespace mtm{
 
-DateWrap& operator+(const DateWrap& date,const int& days);
-DateWrap& operator+(const int& days,const DateWrap& date);
+    class DateWrap{
+        private:
+            Date date;
+
+        public:
+            DateWrap(int day,int month,int year);
+            ~DateWrap();
+            DateWrap(const DateWrap& date_to_copy);
+            int year() const;
+            int month() const;  
+            int day() const;
+            DateWrap& operator+=(const int days);
+            DateWrap& operator++(int);
+            DateWrap& operator=(const DateWrap& date_to_copy);
+            bool operator==(const DateWrap& date_wrap2) const;
+            bool operator>(const DateWrap& date_wrap2) const;
+            bool operator<(const DateWrap& date_wrap2) const;
+            bool operator>=(const DateWrap& date_wrap2) const;
+            bool operator<=(const DateWrap& date_wrap2) const;
+            bool operator!=(const DateWrap& date_wrap2) const;
+            friend ostream& operator<<(ostream& os, const DateWrap& date);
+    };
+
+    DateWrap operator+(const DateWrap& date,const int& days);
+    DateWrap operator+(const int& days,const DateWrap& date);
+
+}
 
 #endif
 
